a73a7a4f1092621637d550e653fb0229154df420
Pm7887 <preetmovaliya9@gmail.com>
2024-06-29 15:06:29 +0530

    commit

--- src/app/(main)/(pages)/workflows/editor/page.tsx
+++ src/app/(main)/(pages)/workflows/editor/page.tsx
@@ -4,6 +4,10 @@ type Props = {}
 
 const Page = (props: Props) => {
   //CHALLENGE: If the user tries to access this route you should send them to their first workflow they have or create one or you can have your own behavior.
+  //CHALLENGE: If the user tries to access this route you should send them to their first workflow they have or create one or you can have your own behavior.//CHALLENGE: If the user tries to access this route you should send them to their first workflow they have or create one or you can have your own behavior.//CHALLENGE: If the user tries to access this route you should send them to their first workflow they have or create one or you can have your own behavior.//CHALLENGE: If the user tries to access this route you should send them to their first workflow they have or create one or you can have your own behavior.
+  //CHALLENGE: If the user tries to access this route you should send them to their first workflow they have or create one or you can have your own behavior.
+  //CHALLENGE: If the user tries to access this route you should send them to their first workflow they have or create one or you can have your own behavior.
+  
   return <div>Page</div>
 }
 
